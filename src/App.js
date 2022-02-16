e88d4b529debc8fec74e737a45696f0331e2be95
Signe Petersen <sign4080@gmail.com>
2022-02-16 19:56:41 +0100

    https://github.com/signekp/business-card.git

--- src/App.js
+++ src/App.js
@@ -3,13 +3,14 @@ import './App.css';
 import Content from './components/Content'
 import Header from './components/Header'
 import Footer from './components/Footer'
-
+import Interest from './components/Interest'
 
 function App() {
   return (
     <div className="container">
       <Header />
       <Content />
+      <Interest />
       <Footer />
     </div>
   );
--- src/components/Header.js
+++ src/components/Header.js
@@ -4,7 +4,16 @@ import Portrait from "../image/image-portrait.jpeg"
 export default function Content() {
     return (
         <div>
-            <img className="image" src={Portrait}></img>
+            <img className="image" src={Portrait} alt="signe"></img>
+            <div className="content">
+                <h1>Signe K. Petersen</h1>
+                <h3>Frontend developer</h3>
+                <a href="https://signepetersen.dk/" target="_blank" rel="noreferrer">signepetersen.dk</a>
+            </div>
+            <div className="buttons">
+                <a className="button-left" href="mailto:sign4080@gmail.com" target="_blank" rel="noreferrer"><i className="fa fa-envelope-o" aria-hidden="true"></i>Email</a>
+                <a className="button-right" href="https://www.linkedin.com/in/signe-petersen-0b7452162/" target="_blank" rel="noreferrer"><i className="fab fa-linkedin"></i> LinkedIn</a>
+            </div>
         </div>
     )
 }
